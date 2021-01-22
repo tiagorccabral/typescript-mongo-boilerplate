7b5cad117b4c683a3f9c4ecef15620b9c1d89ff9
Tiago Cabral <tiagorc.cabral@gmail.com>
2021-01-22 16:17:14 -0300

    :wrench: Reformulate config setup, load and validations

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,24 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import Joi from 'joi';
+import { IConfig } from './Iconfig.interface';
 
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
-console.log(process.env);
+const envVarsSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('production', 'development', 'test'),
+  PORT: Joi.number().default(3000)
+}).unknown();
 
-const config = {
-  env: process.env.NODE_ENV,
-  port: process.env.PORT
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const config: IConfig = {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT
 };
 
 export default config;
