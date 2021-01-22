4f3d79076c3d85a771149fabe760222a4e6e1e0c
Tiago Cabral <tiagorc.cabral@gmail.com>
2021-01-22 16:53:44 -0300

    :lock: Install and config Helmet and Cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
+
+// adds helmet HTTP protection
+app.use(helmet());
+
+// Sets CORS and Accepted Origins
+app.use(cors());
+app.options('*', cors());
 
 export default app;
