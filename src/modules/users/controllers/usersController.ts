3305b7f2754489a48a0e58937fe906fb6ba33a91
Tiago Cabral <tiagorc.cabral@gmail.com>
2021-01-29 22:06:59 -0300

    :sparkles: feat: Create user route, controller and service
    Created an endpoint with router, controller, service and validation to create a new user

--- src/modules/users/controllers/usersController.ts
+++ src/modules/users/controllers/usersController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
+import httpStatus from 'http-status';
 import { catchAsync } from '../../../helpers/catchAsync';
-import User from '../models/user';
+import * as usersService from '../services/users.service';
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
+  const user = await usersService.createUser(req.body);
+  res.status(httpStatus.CREATED).send({ user });
 });
 
 export default {
--- src/modules/users/routes/index.ts
+++ src/modules/users/routes/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import validate from '../../../middlewares/validate';
 import usersController from '../controllers/usersController';
+import * as validations from './validations';
 
 export const router: express.Router = express.Router();
 
-router.route('/create')
-  .post(usersController.createUser);
+router.route('/')
+  .post(validate(validations.createUser), usersController.createUser);
