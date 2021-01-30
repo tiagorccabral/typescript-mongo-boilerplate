2999780c718ddc3c1245950f78062967bc2aa6f6
Tiago Cabral <tiagorc.cabral@gmail.com>
2021-01-29 21:18:14 -0300

    :heavy_plus_sign: feat: Install and configure bodyParser
    Added the lib and configured the propper setup at app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import { router } from './routes/v1';
 
 const app = express();
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // adds helmet HTTP protection
 app.use(helmet());
