d749f10a3226e83bbc62df851daab3501ae13eb7
Tiago Cabral <tiagorc.cabral@gmail.com>
2021-02-01 21:10:47 -0300

    :bug: fix: adjusts healthcheck router name

--- src/modules/healthChecks/routes/index.ts
+++ src/modules/healthChecks/routes/index.ts
@@ -3,5 +3,5 @@ import healthChecksController from '../controllers/healthChecksController';
 
 export const router: express.Router = express.Router();
 
-router.route('/health')
+router.route('/')
   .get(healthChecksController.getHealth);
