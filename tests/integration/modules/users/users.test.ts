ff1e699490b54623e6b87c1c638008e0a90fc3ca
Tiago Cabral <tiagorc.cabral@gmail.com>
2021-02-07 21:01:58 -0300

    :white_check_mark: tests: creates tests for 'create-admin' endpoint

--- tests/integration/modules/users/users.test.ts
+++ tests/integration/modules/users/users.test.ts
@@ -1,4 +1,5 @@
 import request, { Response } from 'supertest';
+import httpStatus from 'http-status';
 import { expect } from 'chai';
 import * as tokenService from '../../../../src/modules/auth/services/token.service';
 import app from '../../../../src/app';
@@ -27,7 +28,7 @@ describe('Users routes', () => {
         });
         it('should return 200', async () => {
           const response: Response = await request(app).get(`/v1/users/${user.id}`).set('authorization', authString);
-          expect(response.status).to.equal(200);
+          expect(response.status).to.equal(httpStatus.OK);
         });
         it('should return own user data', async () => {
           const response: Response = await request(app).get(`/v1/users/${user.id}`).set('authorization', authString);
@@ -46,14 +47,111 @@ describe('Users routes', () => {
         after(async () => {
           await User.deleteMany({});
         });
-        it('should return 401', async () => {
+        it('should return 200', async () => {
           const response: Response = await request(app).get(`/v1/users/${secondUser.id}`).set('authorization', authString);
-          expect(response.status).to.equal(401);
+          expect(response.status).to.equal(httpStatus.OK);
         });
-        it('should return error message', async () => {
+        it('should return different user data', async () => {
           const response: Response = await request(app).get(`/v1/users/${secondUser.id}`).set('authorization', authString);
-          expect(response.text).to.include('You cannot access this content');
+          expect(response.body.user.name).to.equal(secondUser.name);
+          expect(response.body.user.email).to.equal(secondUser.email);
         });
+      });
+    });
+  });
+
+  describe('POST v1/users/create-admin', () => {
+    describe('Current user is logged as an admin user', () => {
+      let user: IUserDoc;
+      let authString: string;
+      describe('creates an admin user', () => {
+        beforeEach(async () => {
+          await User.deleteMany({});
+          user = await User.create({
+            email: 'admin1@email.com', name: 'Admin User', password: 'password123', role: 'admin'
+          });
+          const token: tokenService.IAccessToken = await tokenService.generateAuthTokens(user);
+          authString = `Bearer ${token.access.token}`;
+        });
+        afterEach(async () => {
+          await User.deleteMany({});
+        });
+        it('should return 201', async () => {
+          const response: Response = await request(app).post('/v1/users/create-admin')
+            .set('authorization', authString)
+            .send({ email: 'admin2@email.com', name: 'James Maxwell', password: 'password123' });
+          expect(response.status).to.equal(201);
+        });
+        it('should return new admin user', async () => {
+          const response: Response = await request(app).post('/v1/users/create-admin')
+            .set('authorization', authString)
+            .send({ email: 'admin2@email.com', name: 'James Maxwell', password: 'password123' });
+          expect(response.body.user.name).to.equal('James Maxwell');
+          expect(response.body.user.email).to.equal('admin2@email.com');
+          expect(response.body.user.role).to.equal('admin');
+        });
+      });
+      describe('does not create new admin user', () => {
+        before(async () => {
+          await User.deleteMany({});
+          user = await User.create({
+            email: 'admin1@email.com', name: 'Admin User', password: 'password123', role: 'admin'
+          });
+          const token: tokenService.IAccessToken = await tokenService.generateAuthTokens(user);
+          authString = `Bearer ${token.access.token}`;
+        });
+        after(async () => {
+          await User.deleteMany({});
+        });
+        it('should return 400 if missing fields', async () => {
+          const response: Response = await request(app).post('/v1/users/create-admin')
+            .set('authorization', authString)
+            .send({ email: 'admin2@email.com', password: 'password123' });
+          expect(response.status).to.equal(400);
+        });
+        it('should return error message if e-mail already exists', async () => {
+          const response: Response = await request(app).post('/v1/users/create-admin')
+            .set('authorization', authString)
+            .send({ email: user.email, name: 'New admin', password: 'password123' });
+          expect(response.status).to.equal(400);
+          expect(response.text).to.include('Email already taken');
+        });
+      });
+    });
+
+    describe('Current user is logged as a regular user', () => {
+      let user: IUserDoc;
+      let authString: string;
+      beforeEach(async () => {
+        await User.deleteMany({});
+        user = await User.create({
+          email: 'regular@email.com', name: 'Regular User', password: 'password123'
+        });
+        const token: tokenService.IAccessToken = await tokenService.generateAuthTokens(user);
+        authString = `Bearer ${token.access.token}`;
+      });
+      afterEach(async () => {
+        await User.deleteMany({});
+      });
+      it('should return 401', async () => {
+        const response: Response = await request(app).post('/v1/users/create-admin')
+          .set('authorization', authString)
+          .send({ email: 'admin1@email.com', name: 'James Maxwell', password: 'password123' });
+        expect(response.status).to.equal(401);
+      });
+    });
+
+    describe('Current user is not logged in', () => {
+      beforeEach(async () => {
+        await User.deleteMany({});
+      });
+      afterEach(async () => {
+        await User.deleteMany({});
+      });
+      it('should return 401', async () => {
+        const response: Response = await request(app).post('/v1/users/create-admin')
+          .send({ email: 'admin1@email.com', name: 'James Maxwell', password: 'password123' });
+        expect(response.status).to.equal(401);
       });
     });
   });
